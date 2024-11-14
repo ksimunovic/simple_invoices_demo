088058ff27f618e2a18bafafdcaedadc0dbd1d8d
Karlo Šimunović <karlo.simunovic@gmail.com>
2024-11-14 15:05:54 +0100

    Add validations to Invoice model
    - Ensure `client_name` is present and unique.
    - Validate `amount` is present and non-negative.
    - Allow `tax` to be nil, but if present, ensure it's non-negative.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,10 @@
 class Invoice < ApplicationRecord
   acts_as_paranoid
 
+  validates :client_name, presence: true, uniqueness: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :tax, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+
   def total
     if tax.present?
       amount * (1 + (tax / 100))
