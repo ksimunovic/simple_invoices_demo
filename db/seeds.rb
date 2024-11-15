6db4824d7f7c4d757003baac515c4a90297b31f7
Karlo Šimunović <karlo.simunovic@gmail.com>
2024-11-15 19:22:01 +0100

    Update seeding process for invoices
    - Create clients using Faker::Company
    - Adjusted Invoice creation syntax for clarity
    - Maintained random tax selection logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 10.times do
-  Invoice.create client_name: Faker::Name.name,
+  Invoice.create(client: Client.create(name: Faker::Company.name),
                  amount: Faker::Number.decimal(l_digits: 3, r_digits: 2),
-                 tax: [nil, 8, 12, 30].sample
+                 tax: [nil, 8, 12, 30].sample)
 end
 
 User.create!(email: 'admin@designfiles', password: 'designfiles', password_confirmation: 'designfiles')
